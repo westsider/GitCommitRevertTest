d8dbb69748dddc59b7c8e031c3d15aa831ab71ca
Warren Hansen <whansen1@mac.com>
2020-10-28 10:31:45 -0700

    commit 2

--- GitCommitRevertTest/ViewController.swift
+++ GitCommitRevertTest/ViewController.swift
@@ -13,6 +13,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // commit 1
+        // commit 2
+        /*
+                 i want:
+                 A-B-C-D-E
+                 to become
+                 A-B-C-D-E-F
+                 where F has exactly the same content as C
+                 
+                 git reset --hard HEAD~3
+                 git revert HEAD     # Reverts E
+                 git revert HEAD~2   # Reverts D
+            */
+        
     }
 
 
